3e443d5bb5b130588075fb98ec8f59a23c842d90
Ethan Henderson <ethan.henderson.1998@gmail.com>
2024-04-27 21:41:30 +0100

    Optimise dependencies + set maintainer

--- setup.py
+++ setup.py
@@ -3,13 +3,15 @@ from setuptools import setup
 setup(
     name="touchid",
     py_modules=["touchid"],
-    version="1.2.0",
+    version="2.0.0",
     description="Access the Touch ID sensor",
     long_description="`touchid` provides an easy to use API for accessing the Touch ID sensor via Apple's LocalAuthentication framework",
     keywords=["touch-id", "LocalAuthentication", "LAContext"],
     license="MIT",
     author="Lukas Kollmer",
     author_email="lukas.kollmer@gmail.com",
-    url="https://github.com/lukaskollmer/py-touch-id/",
-    install_requires=["pyobjc"]
+    maintainer="Ethan Henderson",
+    maintainer_email="ethan.henderson.1998@gmail.com",
+    url="https://github.com/parafoxia/python-touch-id/",
+    install_requires=["pyobjc-framework-LocalAuthentication"],
 )
